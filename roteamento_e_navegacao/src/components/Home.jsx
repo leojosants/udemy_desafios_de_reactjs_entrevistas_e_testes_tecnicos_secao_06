a76b10d4d7bcf21a3c37ffb7b7ff760e2d7f34b1
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 15:44:30 -0300

    class 68 - exercise 26: navigation links solution

--- roteamento_e_navegacao/src/components/Home.jsx
+++ roteamento_e_navegacao/src/components/Home.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
             <h2>Início</h2>
+
+            <ul>
+                <li>
+                    <Link to={'/usuarios/1'}>Usuário 1</Link>
+                </li>
+                
+                <li>
+                    <Link to={'/usuarios/2'}>Usuário 2</Link>
+                </li>
+
+                <li>
+                    <Link to={'/usuarios/3'}>Usuário 3</Link>
+                </li>
+            </ul>
         </div>
     );
 };
