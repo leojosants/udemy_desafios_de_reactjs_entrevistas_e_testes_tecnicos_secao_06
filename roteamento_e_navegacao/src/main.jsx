5967ce0092de16c4ce7d0769a931867cb6fad71f
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 14:42:15 -0300

    class 65 - exercise 24: basic navigation solution

---
+++ roteamento_e_navegacao/src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// 1 - basic routing
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+
+// 2 - page configuration
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/contato',
+        element: <Contact />
+      },
+      {
+        path: '/sobre',
+        element: <About />
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
