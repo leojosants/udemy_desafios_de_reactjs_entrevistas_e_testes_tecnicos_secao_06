3c3fd3e11c5e36aa3b4966773e5996389d0c2885
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 16:30:04 -0300

    class 75 - exercise 29: protected route solution

--- roteamento_e_navegacao/src/main.jsx
+++ roteamento_e_navegacao/src/main.jsx
@@ -12,11 +12,15 @@ import About from './components/About';
 // 2 - parameterezid route
 import User from './components/User.jsx';
 
-// 4 - Error route
+// 4 - error route
 import NotFound from './components/NotFound.jsx';
 
 // 5 - search / query params
 import Search from './components/Search.jsx';
+
+// 6 - protected route
+const isAuth = false;
+import { Navigate } from 'react-router-dom';
 
 // page configuration
 const router = createBrowserRouter([
@@ -35,7 +39,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/sobre',
-        element: <About />
+        element: isAuth ? <About /> : <Navigate to={'/'} />
       },
       {
         path: '/usuarios/:userId',
